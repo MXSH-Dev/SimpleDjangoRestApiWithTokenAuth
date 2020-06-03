9c514ec79afc5b8079189ca1127a1155569016aa
MXSH-Dev <x597653559@gmail.com>
2020-06-02 22:29:43 -0600

    added serializers and http methods

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -5,11 +5,15 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
+from rest_framework import status
+from profiles_api import serializers
+
 
 class HelloApiView(APIView):
     '''
     Test API View
     '''
+    serializer_class = serializers.HellowSerializer
 
     def get(self, request, format=None):
         '''
@@ -23,3 +27,38 @@ class HelloApiView(APIView):
         ]
 
         return Response({"message": "hello", 'an_apiview': an_apiview})
+
+    def post(self, request):
+        '''
+        Create a hello message with our name
+        '''
+
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello {name}'
+            return Response({'message': message})
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def put(self, request, pk=None):
+        '''
+        Handle updating an object
+        '''
+
+        return Response({'methond': 'PUT'})
+
+    def patch(self, request, pk=None):
+        '''
+        Handle a partial update of an object
+        '''
+
+        return Response({'methond': 'PATCH'})
+
+    def delete(self, request, pk=None):
+        '''
+        Handle a partial update of an object
+        '''
+
+        return Response({'methond': 'DELETE'})
